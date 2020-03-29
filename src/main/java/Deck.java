06c5365ff69fcfb4ec0c89b5f667024a7e7cb4a5
mholwill <m_holwill@hotmail.co.uk>
2020-03-29 20:45:53 +0100

    Create deck of cards added

--- src/main/java/Deck.java
+++ src/main/java/Deck.java
@@ -1,2 +1,30 @@
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.ArrayList;
+
 public class Deck {
+
+private ArrayList<Card> cards;
+
+
+    public Deck(){
+        this.cards = new ArrayList<Card>();
+    }
+
+    public void createDeck(){
+        cards = new ArrayList<Card>();
+        for (SuitType suit : SuitType.values()){
+            for (RankType rank : RankType.values()){
+                Card card = new Card(suit, rank);
+                cards.add(card);
+            }
+        }
+    }
+
+
+    public int getCards() {
+        return this.cards.size();
+    }
+
 }
--- src/test/java/DeckTest.java
+++ src/test/java/DeckTest.java
@@ -1,4 +1,32 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class DeckTest {
+
+    Deck deck;
+
+    @Before
+    public void setUp(){
+        deck = new Deck();
+    }
+
+    @Test
+    public void checkDeckEmpty(){
+        assertEquals(0, deck.getCards());
+    }
+
+    @Test
+    public void checkDeckPopulated(){
+        deck.createDeck();
+        assertEquals(52, deck.getCards());
+    }
+
+
+
+
+
 
 
 }
